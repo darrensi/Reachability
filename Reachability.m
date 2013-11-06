0a8d560b6aff0dc40902a1211138a6ebf52e36a7
Darren <darren@mmggames.com>
2013-11-06 12:08:56 -0600

    Renamed symbol to avoid conflict

--- Reachability.m
+++ Reachability.m
@@ -28,7 +28,7 @@
 #import "Reachability.h"
 
 
-NSString *const kReachabilityChangedNotification = @"kReachabilityChangedNotification";
+NSString *const __kReachabilityChangedNotification = @"kReachabilityChangedNotification";
 
 @interface Reachability ()
 
@@ -510,7 +510,7 @@ static void TMReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkRea
     
     // this makes sure the change notification happens on the MAIN THREAD
     dispatch_async(dispatch_get_main_queue(), ^{
-        [[NSNotificationCenter defaultCenter] postNotificationName:kReachabilityChangedNotification 
+        [[NSNotificationCenter defaultCenter] postNotificationName:__kReachabilityChangedNotification 
                                                             object:self];
     });
 }
